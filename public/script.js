7a8a15d272d2bdb0aa4d9c3736fdd3723cbc7411
cryst0f <krystofmikeska1@gmail.com>
2025-04-09 16:37:27 -0500

    quiz w no logic

--- public/script.js
+++ public/script.js
@@ -57,6 +57,7 @@ const nextButton = document.getElementById("next-btn");
 const prevButton = document.getElementById("prev-btn");
 
 let currentQuestionIndex = 0;
+let selectedAnswers = Array(questions.length).fill(null); 
 
 function startQuiz(){
     currentQuestionIndex = 0;
@@ -65,18 +66,76 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + 
-    currentQuestion.question;
+    currentQuestion.question; //1
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
+
+        if (selectedAnswers[currentQuestionIndex] === answer.text){
+            button.classList.add("active");
+        }
+
         answerButtons.appendChild(button);
-    })
+        button.addEventListener("click", selectAnswer);
+         
+    });
 
 }
+
+function resetState(){
+    while (answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+
+    const buttons = answerButtons.querySelectorAll("button");
+    buttons.forEach(btn => btn.classList.remove("active"));
+
+    selectedBtn.classList.add("active");
+    selectedAnswers[currentQuestionIndex] = selectedBtn.innerHTML;
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        startQuiz();
+    }
+}
+
+function handlePrevButton(){
+    if (currentQuestionIndex > 0){
+        currentQuestionIndex--;
+        showQuestion();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if (currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+prevButton.addEventListener("click", ()=>{
+    if (currentQuestionIndex < questions.length){
+        handlePrevButton();
+    }else{
+        startQuiz();
+    }
+});
+
 
 startQuiz();
