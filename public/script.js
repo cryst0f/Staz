eb2af0847e5d47713a8ba396749475afa20bdd61
cryst0f <krystofmikeska1@gmail.com>
2025-04-11 23:21:34 +0200

    add notes to questions

--- public/script.js
+++ public/script.js
@@ -3,8 +3,8 @@ const questions = [
         question: "V jakém odvětví působíte",
         type: "select",
         answers: [
-            { text: "Digitální infrastruktura a služby"},
-            { text: "Drážní doprava"},
+            { text: "Digitální infrastruktura a služby", value: 1},
+            { text: "Drážní doprava", value: 0},
             { text: "Energetika - Elektřina"},
             { text: "Energetika - Plynárenství"},
             { text: "Energetika - Ropa a ropné produkty"},
@@ -25,47 +25,60 @@ const questions = [
             { text: "Vojenský průmysl"},
             { text: "Vodní průmysl"},
             { text: "Zdravotnictví"}
-        ]
+        ],
+        evaluate: (answer, question) => {
+            const selectedAnswer = question.answers.find(a => a.text === answer);
+            return selectedAnswer ? { points: selectedAnswer.value } : { points: 0 };
+        }
     },
 
     //na zaklade prvni otazky -> otazka na sluzbu
     {
-        question: "Jakou službu posktujete ?",
+        question: "Jakou službu poskytujete",
         type: "select",
         dependsOn:{
             questionIndex: 0,
             value: "Digitální infrastruktura a služby"
         },
         answers: [
-            {text: "Poskytování platformy sociální sítě"},
-            {text: "Poskytování řízené bepečnostní služby"},
-            {text: "Poskytování služby cloud computingu"},
-            {text: "Poskytování služby datového centra"},
-            {text: "Poskytování služby internetového vysledávače"},
-            {text: "Poskytování služby on-line tržiště"},
-            {text: "Poskytování služby sítě pro doručování obsahu"},
-            {text: "Poskytování služby systému překladu doménových jmen"},
-            {text: "Poskytování služby výměnného uzlu"},
-            {text: "Poskytování služby vytváření důvěry"},
-            {text: "Poskytování veřejně dostupné službz elektronických komunikací"},
-            {text: "Provozování Národního CERT"},
-            {text: "Správa a provoz registru domény nejvyšší úrovně"},
-            {text: "Správa kvalifikovaného systému elektronické identifikace"},
-            {text: "Zajišťování veřejné komunikační"}
+            {text: "Poskytování platformy sociální sítě",value: 1}, // bez add question nic, s add <250 nizsi, >250 vyssi
+            {text: "Poskytování řízené bepečnostní služby", value: 1}, //bez add question nic, s add <250 nizsi, >250 vyssi
+            {text: "Poskytování řízené služby", value: 0}, // bez add question nic, s add <250 nizsi, >250 vyssi
+            {text: "Poskytování služby cloud computingu", value: 1}, // bez <250 nizsi >250 vyssi, s add vsechno vyssi
+            {text: "Poskytování služby datového centra",value: 0}, //bez add question nic, s add <250 nizsi, >250 vyssi
+            {text: "Poskytování služby internetového vysledávače", value: 0}, //bez add question nic, s add <250 nizsi a taky >250 nizsi !!!!
+            {text: "Poskytování služby on-line tržiště", value: 0}, //bez add question nic, s add <250 nizsi a taky >250 nizsi !!!!
+            {text: "Poskytování služby sítě pro doručování obsahu",value: 0}, //bez add question nic, s add <250 nizsi, >250 vyssi
+            {text: "Poskytování služby systému překladu doménových jmen", value: 0}, //bez special nic, toto ma pouze special question, kde v obou pripdaech (jsou to ano questions) je vyssi
+            {text: "Poskytování služby výměnného uzlu", value: 0}, // opet special question bez ni nic, s ni vyssi
+            {text: "Poskytování služby vytváření důvěry",value: 0},// toto ma 2 special questions, bez nic, pokud plati prvni tak nic, pokud plati 2 tak u vsech po <250 nizsi pro > 250 vyssi, pokud plati obe tak vsechny pripady vyssi
+            {text: "Poskytování veřejně dostupné služby elektronických komunikací",value: 1},// ma opet 2 special questions, bez nich pro <50 jsou nizsi, potom od 250 vyssi, pokud plati 1 question tak u vsecho vyssi, to stejne u druhe a logicky obe vyssi
+            {text: "Provozování Národního CERT",value: 0},// pouze special otazka, pokud ne nic, pokud ano vyssi 
+            {text: "Správa a provoz registru domény nejvyšší úrovně",value: 0}, // pouze special otazka, pokud ne nic, pokud ano vyssi 
+            {text: "Správa kvalifikovaného systému elektronické identifikace",value: 0}, // pouze special otazka, pokud ne nic, pokud ano vyssi 
+            {text: "Zajišťování veřejné komunikační",value: 1} // ma 2 special otazky, bez nich <50 nizsi ostatni vyssi, pokud plati 1 otazka vse vyssi, stejne u druhe
 
-        ]
+        ],
+        evaluate: (answer, question) => {
+            const selectedAnswer = question.answers.find(a => a.text === answer);
+            return selectedAnswer ? { points: selectedAnswer.value } : { points: 0 };
+        }
     },
 
-
+    //oddeleni druhych otazek
     {
         question: "Kolik má vaše společnost zaměstnanců",
         type: "buttons",
         answers: [
             {text: "Méně jak 10 zaměstnanců"},
             {text: "Méně jak 50 zaměstnanců"},
-            {text: "Méně jak 250 zaměstnanců"},
-            {text: "Více jak 250 zaměstnanců"}     
-        ]
+            {text: "Méně jak 250 zaměstnanců",value: 0},
+            {text: "Více jak 250 zaměstnanců",value: 1}     
+        ],
+        evaluate: (answer, question) => {
+            const selectedAnswer = question.answers.find(a => a.text === answer);
+            return selectedAnswer ? { points: selectedAnswer.value } : { points: 0 };
+        }
     },
     {
         question: "Jaký je váš roční obrat nebo suma bilanční rozvahy",
@@ -73,9 +86,13 @@ const questions = [
         answers: [
             {text: "Obrat menší jak 2 miliony EUR"},
             {text: "Obrat menší jak 10 milionů EUR"},
-            {text: "Obrat menší jak 50 milionů EUR nebo rozvaha menší jak 43 milionů EUR"},
-            {text: "Obrat větší jak 50 milionů EUR nebo rozvaha větší jak 43 milionů EUR"}       
-        ]
+            {text: "Obrat menší jak 50 milionů EUR nebo rozvaha menší jak 43 milionů EUR",value: 0},
+            {text: "Obrat větší jak 50 milionů EUR nebo rozvaha větší jak 43 milionů EUR",value: 1}       
+        ],
+        evaluate: (answer, question) => {
+            const selectedAnswer = question.answers.find(a => a.text === answer);
+            return selectedAnswer ? { points: selectedAnswer.value } : { points: 0 };
+        }
     }
 
     
@@ -102,13 +119,12 @@ function showQuestion(){
 
     let currentQuestion = questions[currentQuestionIndex];
 
-    // Kontrola závislosti otázky
     if (currentQuestion.dependsOn) {
         const { questionIndex, value } = currentQuestion.dependsOn;
         const selected = selectedAnswers[questionIndex];
 
         if (selected !== value) {
-            handleNextButton(); // Přeskočí otázku
+            handleNextButton(); 
             return;
         }
     }
@@ -145,14 +161,14 @@ function showQuestion(){
       answerButtons.appendChild(selectBox);
       const selectContainer = document.querySelector('.select-box');
       if (selectContainer) {
-        selectContainer.style.display = "block"; // Ukáže select box a šipku
+        selectContainer.style.display = "block"; 
 
         selectBox.addEventListener("change", function() {
             selectedAnswers[currentQuestionIndex] = this.value;
         });
       }
     }
-    prevButton.disabled = history.length <= 1; // Zakázat "Předchozí" na první otázce
+    prevButton.disabled = history.length <= 1; 
     nextButton.innerHTML = currentQuestionIndex === questions.length - 1 ? "Dokončit" : "Dalsi";
 }
 
@@ -199,22 +215,40 @@ function handleNextButton(){
 
     if (nextIndex < questions.length) {
         currentQuestionIndex = nextIndex;
-        history.push(currentQuestionIndex); // Přidáme zobrazenou otázku do historie
+        history.push(currentQuestionIndex); 
         showQuestion();
     } else {
-        startQuiz(); // Nebo zobrazíme výsledky, pokud jsme na konci
+        startQuiz(); 
     }
 }
 
 function handlePrevButton(){
     if (history.length > 1) {
-        history.pop(); // Odstraníme aktuální otázku z historie
-        currentQuestionIndex = history[history.length - 1]; // Nastavíme předchozí otázku
+        history.pop(); 
+        currentQuestionIndex = history[history.length - 1]; 
         showQuestion();
     }
 }
 
 nextButton.addEventListener("click", handleNextButton);
 prevButton.addEventListener("click", handlePrevButton);
+
+function evaluateQuiz(questions, answers) {
+    let totalScore = 0;
+    const results = {};
+
+    questions.forEach((question, index) => {
+        const userAnswer = answers[index];
+        if (question.evaluate && userAnswer !== null) {
+            const evaluationResult = question.evaluate(userAnswer, question); // Předáváme i samotnou otázku
+            results[`question${index + 1}`] = evaluationResult || {};
+            totalScore += evaluationResult?.points || 0;
+        } else {
+            results[`question${index + 1}`] = { points: 0 };
+        }
+    });
+
+    return { totalScore, results };
+}
 
 startQuiz();
